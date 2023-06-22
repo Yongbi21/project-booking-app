e269f2388a28b7220ca6509410c17fc5f256f3b9
Jerimiedot <jerimietevesulgasan@gmail.com>
2023-06-22 09:39:32 +0800

    breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
-require('./bootstrap');
-import {createApp} from 'vue'
-import App from './App.vue'
-createApp(App).mount("#app");
+import './bootstrap';
 
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
